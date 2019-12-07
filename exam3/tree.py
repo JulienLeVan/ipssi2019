f98f2aa39164db3ecf7b6cb89ad6f5b5b16eae0c
JulienLeVan <levan.julienn@gmail.com>
2019-12-07 01:15:38 +0100

    update

--- exam3/tree.py
+++ exam3/tree.py
@@ -25,16 +25,17 @@ def show_tree(n):
     # BRANCHES
     for i in range(p):
         print(" "*(p-1-i) + 'X' * ( (2*i+1)))    
-        
+    
+
     # TRONC
     if tronc == 1:
         for i in range(hauteur):
             print(" " *( (ceil(largeur/2))-1 ) + "X")
+
     else:
         for i in range(hauteur):
             print(" "*( (floor(largeur/2))-1 )  + "XXX")
-
-
+    
+    
 if __name__ == "__main__":
-    out = (show_tree(int(sys.argv[1])))
-    print(out)
+    (show_tree(int(sys.argv[1])))
